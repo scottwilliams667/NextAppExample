ed7e37177aee3a2bae917c19bc0b88886bfb77db
scottwilliams667 <58715209+scottwilliams667@users.noreply.github.com>
2023-09-17 10:56:43 -0700

    change text

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h3>{`Scott's Page!`}</h3>
+      <h3>{`Scott's Page on develop!`}</h3>
     </main>
   )
 }
